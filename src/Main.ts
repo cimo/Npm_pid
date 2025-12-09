8cd59e37c90f8a8b4a8e66e057ed56d1c1376f71
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-12-09 19:51:11 +0900

    update

--- src/Main.ts
+++ src/Main.ts
@@ -1,4 +1,4 @@
-import Cp from "./Manager";
+import Cp from "./Manager.js";
 
-export * as CpModel from "./Model";
+export * as CpModel from "./Model.js";
 export { Cp };
--- src/Manager.ts
+++ src/Manager.ts
@@ -1,5 +1,5 @@
 // Source
-import * as model from "./Model";
+import * as model from "./Model.js";
 
 export default class Manager {
     private mainObject: Record<number, model.Iparameter>;
